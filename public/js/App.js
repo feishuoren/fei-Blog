207c873c145dc8f80a4ac37e8ef981d86fdadef0
feishuoren <angela335889390@gmail.com>
2022-12-03 16:07:57 +0800

    add mousedown&mouseup

--- public/js/App.js
+++ public/js/App.js
@@ -5,14 +5,20 @@ import Navigation from './Navigation'
 import Content from './Content'
 
 import '!style-loader!css-loader!../styles/style.css'
-import {handleMouseMove} from './mouseHandler'
+import { handleMouseMove, handleClick, handleMouseDown, handleMouseUp } from './mouseHandler'
 
 export default function App() {
   useEffect(() => {
     document.body.addEventListener('mousemove',handleMouseMove)
+    document.body.addEventListener('click',handleClick)
+    document.body.addEventListener('mousedown',handleMouseDown)
+    document.body.addEventListener('mouseup',handleMouseUp)
 
     return () => {
       document.body.removeEventListener('mousemove',handleMouseMove)
+      document.body.removeEventListener('click',handleClick)
+      document.body.removeEventListener('mousedown',handleMouseDown)
+      document.body.removeEventListener('mouseup',handleMouseUp)
     }
   }, [])
 
--- public/js/mouseHandler.js
+++ public/js/mouseHandler.js
@@ -1,13 +1,14 @@
 export function handleMouseMove(e) {
   let body = document.querySelector('body')
-  let bubbles = document.createElement('span')
+  let bubbles = document.createElement('div')
   let x = e.clientX
   let y = e.clientY
+  bubbles.className = 'kitty';
 
   bubbles.style.left = x + 'px'
   bubbles.style.top = y + 'px'
 
-  let size = Math.random() * 50
+  let size = Math.random() * 30
   bubbles.style.width = size + 'px'
   bubbles.style.height = size + 'px'
 
@@ -18,3 +19,82 @@ export function handleMouseMove(e) {
   },2000)
 
 }
+
+export function handleClick(e) { 
+
+}
+
+export function handleMouseDown(e) {
+  let body = document.querySelector('body')
+
+  // 点击爱心特效
+  // 文字数组
+  const text = ['hi~', '点点点~', '啦啦啦~', '干嘛呀~', '哦吼~', '咦~'];
+
+  // 获取鼠标点击位置
+  let x = e.clientX - 18
+  let y = e.clientY - 30
+
+  // 分别创建所需要的元素节点
+  let heart_box = document.createElement('div')
+  let heart_left = document.createElement('div')
+  let heart_right = document.createElement('div')
+  let heart_under = document.createElement('div')
+  let heart_txt = document.createElement('div')
+
+  // 通过随机数从文字数组中获取随机下标的文字
+  let textNode = document.createTextNode(text[parseInt(Math.random() * text.length)]);
+
+  // 文字添加到txt节点中
+  heart_txt.appendChild(textNode);
+
+  heart_box.className = 'heart_box';
+  heart_left.className = 'heart_left';
+  heart_right.className = 'heart_right';
+  heart_under.className = 'heart_under';
+  heart_txt.className = 'heart_text';
+
+  heart_box.style.top = y + 'px';
+  heart_box.style.left = x + 'px';
+
+  // 将创建的元素添加到容器中
+  heart_box.appendChild(heart_left);
+  heart_box.appendChild(heart_right);
+  heart_box.appendChild(heart_under);
+  heart_box.appendChild(heart_txt);
+  document.body.appendChild(heart_box);
+  
+  // 获取随机颜色
+  let color = 'rgb(' + 
+    parseInt(Math.random() * 255) + ',' + 
+    parseInt(Math.random() * 255) + ',' + 
+    parseInt(Math.random() * 255) + ')'
+  heart_txt.style.color = color
+
+  for (let i = 0; i < 3; i++) {
+      heart_box.children[i].style.background = color
+  }
+
+}
+
+
+export function handleMouseUp(e) {
+  // 爱心消失删除
+  const boxs = document.getElementsByClassName('heart_box')
+
+  for (let i = 0; i < boxs.length; i++) {
+    boxs[i].style.transform = 'scale(0.5)'
+    boxs[i].style.transform = 'translateY(-40px)'
+    boxs[i].style.opacity = '0'
+
+    setTimeout(() => {
+     if (boxs[i]) {
+       boxs[i].remove()
+     }
+    },2000)
+  }
+
+}
+
+
+
